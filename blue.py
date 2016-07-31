1f202dd796fec3ecad3893c242d91d7cebc960d9
Raaghav0 <es15btech11021@iith.ac.in>
2016-07-31 14:09:28 +0530

    Update blue.py

--- blue.py
+++ blue.py
@@ -1,1 +1,22 @@
+import bluetooth
 
+target_name = "HC-05" 
+target_address = None
+
+nearby_devices = bluetooth.discover_devices()
+
+for bdaddr in nearby_devices:
+	if target_name == bluetooth.lookup_name( bdaddr ):
+	        target_address = bdaddr	
+		break
+	
+
+
+port = 1
+
+sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+sock.connect((target_address, port))
+
+sock.send("H")
+
+sock.close()
